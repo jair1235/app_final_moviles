7d0cb47ec4d7115cce4ba76d284203c82f33d964
JairDop <jairneuta512@gmail.com>
2025-09-24 21:07:02 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,33 +2,31 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
     path: 'tabs',
-    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
-  },
-  {
-    path: 'inicio',
-    loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
-  },
-  {
-    path: 'educacion',
-    loadComponent: () => import('./pages/educacion/educacion.page').then( m => m.EducacionPage)
-  },
-  {
-    path: 'presupuesto',
-    loadComponent: () => import('./pages/presupuesto/presupuesto.page').then( m => m.PresupuestoPage)
-  },
-    {
+    loadComponent: () => import('./tabs/tabs.page').then(m => m.TabsPage),
+    children: [
+      {
+        path: 'inicio',
+        loadComponent: () => import('./pages/inicio/inicio.page').then(m => m.InicioPage),
+      },
+      {
+        path: 'educacion',
+        loadComponent: () => import('./pages/educacion/educacion.page').then(m => m.EducacionPage),
+      },
+      {
+        path: 'presupuesto',
+        loadComponent: () => import('./pages/presupuesto/presupuesto.page').then(m => m.PresupuestoPage),
+      },
+      {
         path: '',
         redirectTo: '/tabs/inicio',
         pathMatch: 'full',
       },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full',
+  },
 ];
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabButton, IonTabs, IonTabBar, IonIcon, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonIcon, IonTabBar, IonTabs, IonTabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, RouterModule, CommonModule], // 👈 IMPORTANTE
 })
-export class TabsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class TabsPage {}
